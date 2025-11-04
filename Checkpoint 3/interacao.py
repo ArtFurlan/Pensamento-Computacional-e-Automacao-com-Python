817f6c918a7846ee8c15b32705a04e26ff193a28
ArtFurlan <arthurmfurlan@gmail.com>
2025-11-03 21:14:58 -0300

    Delete Checkpoint 3 directory

--- Checkpoint 3/interacao.py
+++
@@ -1,8 +0,0 @@
-class Interacao:
-    def __init__(self, tipo, descricao, responsavel):
-        self.tipo = tipo
-        self.descricao = descricao
-        self.responsavel = responsavel
-
-    def exibir_informacoes(self):
-        print(f"  - Tipo: {self.tipo} | Descrição: {self.descricao} | Responsável: {self.responsavel}")
